fe2e6cb14587a649140d7f65897b194987d3954f
Wesley Cesar <wesley@criasoft.com.br>
2016-03-22 11:35:58 -0300

    - Adicionando o header - Adicionando o footer

---
+++ header.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+    <title>Primeira Pagina </title>
+    <meta charset="utf-8">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Chamadas JS -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<form action="" name="_form" method="post" enctype="multipart/form-data">
+
+    <nav role="navigation" class="navbar navbar-default">
+
+        <div class="navbar-header">
+            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                <span class="sr-only">Navegação Responsiva</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="#" class="navbar-brand">Home</a>
+        </div>
+    </nav>
